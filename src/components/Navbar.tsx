20b6ba526b75d466e2cbd794871fe4697146a16a
Steven Fernandez <fernandezsteven185@gmail.com>
2024-07-26 07:08:31 -0500

    feat: Finish navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,30 @@
 'use client'
+import { Oooh_Baby } from 'next/font/google'
 
 import { useState } from 'react'
-import { FaBars } from 'react-icons/fa'
-
+import { FaBars, FaLinkedin } from 'react-icons/fa'
 import { Button } from './ui/button'
+import { Github } from './icons'
+const inter = Oooh_Baby({ weight: '400', subsets: ['latin'] })
+
+const navItems = [
+  {
+    title: 'Home',
+    link: '/'
+  },
+  {
+    title: 'Projects',
+    link: '/projects'
+  },
+  {
+    title: 'About',
+    link: '/about'
+  },
+  {
+    title: 'Contact',
+    link: '/contact'
+  }
+]
 
 export default function Navbar () {
   const [mobileMenu, setMobileMenu] = useState(false)
@@ -12,37 +33,30 @@ export default function Navbar () {
     <>
       <nav className='mx-auto w-11/12 py-8 md:w-10/12 lg:max-w-screen-xl'>
         <div className='flex items-center justify-between'>
-          <div>
-            <a href='/'>
-              <img
-                width={80}
-                height='auto'
-                src='/logo.webp'
-                alt='The logo of the joint smoke store'
-              />
-            </a>
-          </div>
+          <h1 className={`${inter.className} text-4xl font-bold`}>Steven</h1>
           <ul className='hidden text-center md:flex md:gap-4'>
-            <li className='text-lg'>
-              <button className='opacity-80 transition-opacity duration-300 hover:opacity-100'>
-                Shop now
-              </button>
-            </li>
-            <li className='text-lg'>
-              <button className='opacity-80 transition-opacity duration-300 hover:opacity-100'>
-                Contact
-              </button>
-            </li>
-            <li className='text-lg'>
-              <a
-                className='opacity-80 transition-opacity duration-300 hover:opacity-100'
-                href='#products'
-              >
-                Products
-              </a>
-            </li>
+            {navItems.map((item, index) => (
+              <li key={index} className='text-lg'>
+                <a
+                  href={item.link}
+                  className='opacity-80 transition-opacity duration-300 hover:opacity-100'
+                >
+                  {item.title}
+                </a>
+              </li>
+            ))}
           </ul>
           <div>
+            <div className='hidden items-center gap-3 md:flex'>
+              <a target='_blank' href='https://www.linkedin.com/in/steven-fernandez-11a522274/'>
+                <FaLinkedin className='text-3xl opacity-60 transition-opacity duration-200 hover:opacity-100 ' />
+              </a>
+              <a target='_blank' href='https://github.com/vengxdor'>
+                <Github className='text-3xl opacity-60 transition-opacity duration-200 hover:opacity-100 ' />
+              </a>
+              {/* Todo: Add resume */}
+              <Button className=''>Resume</Button>
+            </div>
             <button
               onClick={() => {
                 setMobileMenu(!mobileMenu)
@@ -51,30 +65,39 @@ export default function Navbar () {
             >
               <FaBars />
             </button>
-
-            <Button className='hidden rounded-md md:block'>Shop</Button>
           </div>
         </div>
         {/* mobile */}
         {!!mobileMenu && (
-          <ul className='mt-4 flex flex-col gap-4 text-center md:hidden '>
-            <li className='text-lg'>
-              <button className='opacity-80 transition-opacity duration-300 hover:opacity-100'>
-                Shop now
-              </button>
-            </li>
-            <li className='text-lg'>
-              <button className='opacity-80 transition-opacity duration-300 hover:opacity-100'>
-                Contact
-              </button>
-            </li>
-            <li className='text-lg'>
-              <a
-                className='opacity-80 transition-opacity duration-300 hover:opacity-100'
-                href='#products'
-              >
-                Products
-              </a>
+          <ul className='mt-4 flex flex-col gap-4 text-center md:hidden'>
+            {navItems.map((item, index) => (
+              <li key={index} className='text-lg'>
+                <a
+                  href={item.link}
+                  className='opacity-80 transition-opacity duration-300 hover:opacity-100'
+                >
+                  {item.title}
+                </a>
+              </li>
+            ))}
+            <li>
+              <div className='flex flex-col items-center gap-3 md:hidden'>
+                <div className='flex'>
+                  <a
+                    target='_blank'
+                    href='https://www.linkedin.com/in/steven-fernandez-11a522274/'
+                  >
+                    <FaLinkedin className='text-3xl opacity-60 transition-opacity duration-200 hover:opacity-100 ' />
+                  </a>
+                  <a target='_blank' href='https://github.com/vengxdor'>
+                    <Github className='text-3xl opacity-60 transition-opacity duration-200 hover:opacity-100 ' />
+                  </a>
+                </div>
+                {/* Todo: Add resume */}
+                <Button size='lg'>
+                  Resume
+                </Button>
+              </div>
             </li>
           </ul>
         )}
