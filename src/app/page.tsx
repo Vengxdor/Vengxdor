6aa109d1d7e1a8dd04e67a5d3c9ba513b98f9df4
Steven Fernandez <fernandezsteven185@gmail.com>
2024-07-29 05:25:21 -0500

    feat: Footer done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import Hero from '@/components/Hero'
 import Navbar from '@/components/Navbar'
 import Projects from '@/components/Projects'
@@ -12,6 +13,7 @@ export default function Home () {
         <Projects />
         <Skills />
       </main>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer () {
+  return (
+    <footer className='border-t border-white/20'>
+      <p className='p-4 pt-6 text-center opacity-60'>
+        © 2024 <a target='_blank' className='hover:underline' href='https://github.com/vengxdor'>Steven Fernandez</a>
+      </p>
+    </footer>
+  )
+}
