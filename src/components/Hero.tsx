2ab763d28b6440ed35be82aa0acc1fe285b3ec81
Steven Fernandez <fernandezsteven185@gmail.com>
2024-07-31 12:42:02 -0500

    fix: Colors

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero () {
       <img className='rounded-md' src='/author2.png' alt='' />
       <article className='mt-3 grid gap-8 text-center'>
         <h1 className='max-w-[24ch] text-4xl font-bold leading-tight sm:text-5xl md:text-title'>
-          Transforming <span className='text-primary'>Concepts</span> into
+          Transforming <span className='text-secondary'>Concepts</span> into
           Engaging Websites
         </h1>
         <p className='text-sm opacity-70 md:text-base'>
