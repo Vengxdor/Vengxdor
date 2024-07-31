f8d68410fbffe26ae42cf107ab9712205442d9d6
Steven Fernandez <fernandezsteven185@gmail.com>
2024-07-31 07:17:09 -0500

    fix: Add image in contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,13 +7,14 @@ export default function Contact () {
   return (
     <section id='contact' className='mb-10 mt-32 '>
       <h2 className='mb-9 text-center text-subtitle font-bold'>Contact</h2>
-      <div className='flex gap-5'>
-        <form className='grid w-1/2 gap-3'>
+      <div className='flex items-center gap-12'>
+        <form className='grid h-72 w-1/2 items-center '>
           <Input type='text' placeholder='Name'/>
           <Input type='Email' placeholder='Email'/>
           <Textarea className='rezize-none h-32' placeholder='Message...'/>
-          <Button type='submit'>Send</Button>
+          <Button className='bg-secondary' type='submit'>Send</Button>
         </form>
+        <img className='w-1/3' src="/mail.png" alt="" />
       </div>
     </section>
   )
