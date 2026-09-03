a8f5feb98cf858b9697ba4c47f1edf38dba48355
Steven <Fernandezsteven185@gmail.com>
2026-09-03 14:49:02 -0500

    fix: Update button outline color and remove Skills component from index page

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap hover:-translate-y-[2px] transition-transform duration-200 rounded-md text-[15px] font-medium transition-colors focus-visible:outline-none focus-visible:outline-2 disabled:pointer-events-none disabled:opacity-50 focus-visible:outline-purple-500',
+  'inline-flex items-center justify-center whitespace-nowrap hover:-translate-y-[2px] transition-transform duration-200 rounded-md text-[15px] font-medium transition-colors focus-visible:outline-none focus-visible:outline-2 disabled:pointer-events-none disabled:opacity-50 focus-visible:outline-primary',
   {
     variants: {
       variant: {
